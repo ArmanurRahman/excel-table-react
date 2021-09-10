db8a86ef98124c0c6ac1db5bfd74b1300b1b32ec
Armanur Rahman <mubeenarman19@gmail.com>
2021-09-11 00:59:39 +0900

    rename column and row

--- src/App.js
+++ src/App.js
@@ -2,29 +2,29 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 
 const data = {
-    2019: {
-        "01": 0.0002,
-        "02": 0.0004,
-        "03": 0.0004,
-        "04": 0.0004,
-        "05": 0.0004,
-        "06": 0.0004,
+    1: {
+        A: 0.0002,
+        B: 0.0004,
+        C: 0.0004,
+        D: 0.0004,
+        E: 0.0004,
+        F: 0.0004,
     },
-    2020: {
-        "01": 0.0009,
-        "02": 0.2154,
-        "03": 0.0004,
-        "04": 0.0004,
-        "05": 0.0004,
-        "06": 0.0004,
+    2: {
+        A: 0.0009,
+        B: 0.2154,
+        C: 0.0004,
+        D: 0.0004,
+        E: 0.0004,
+        F: 0.0004,
     },
-    2021: {
-        "01": 0.0002,
-        "02": 0.0004,
-        "03": 0.0004,
-        "04": 0.0004,
-        "05": 0.0004,
-        "06": 0.0004,
+    3: {
+        A: 0.0002,
+        B: 0.0004,
+        C: 0.0004,
+        D: 0.0004,
+        E: 0.0004,
+        F: 0.0004,
     },
 };
 function App() {
@@ -43,65 +43,63 @@ function App() {
         };
     }, []);
 
-    const handleCurrentCell = (pcc, month) => {
-        setSelectCell({ [pcc + month]: true });
+    const handleCurrentCell = (row, col) => {
+        setSelectCell({ [row + col]: true });
     };
 
-    const mouseDown = (pcc, month) => {
-        setSelectCell({ [pcc + month]: true });
-        console.log(" mouse down ", pcc, month);
+    const mouseDown = (row, col) => {
+        setSelectCell({ [row + col]: true });
+        console.log(" mouse down ", row, col);
         setIsMouseDown(true);
 
         const slt = [...selectStack];
 
-        slt.push(pcc + month);
+        slt.push(row + col);
         setSelectStack(slt);
     };
 
-    const mouseOver = (pcc, month) => {
+    const mouseOver = (row, col) => {
         if (isMouseDown) {
-            console.log(" mouse over ", pcc, month);
+            console.log(" mouse over ", row, col);
             let select = { ...selectCell };
 
             const slt = [...selectStack];
 
-            if (slt[slt.length - 2] == pcc + month) {
+            if (slt[slt.length - 2] == row + col) {
                 delete select[slt[slt.length - 1]];
                 slt.pop();
                 setSelectStack(slt);
             } else {
-                select = { ...selectCell, [pcc + month]: true };
-                slt.push(pcc + month);
+                select = { ...selectCell, [row + col]: true };
+                slt.push(row + col);
                 setSelectStack(slt);
             }
             setSelectCell(select);
         }
     };
 
-    const mouseOut = (pcc, month) => {
-        console.log(" mouse out ", pcc, month);
+    const mouseOut = (row, col) => {
+        console.log(" mouse out ", row, col);
     };
     const mouseUp = () => {
         console.log(" mouse up ");
         setIsMouseDown(false);
     };
-    const CustomCell = ({ pcc, month }) => {
+    const CustomCell = ({ row, col }) => {
         return (
             <React.Fragment>
                 <td
                     className={
-                        selectCell[pcc + month] ? "td_selected" : undefined
+                        selectCell[row + col] ? "td_selected" : undefined
                     }
-                    //onClick={() => handleCurrentCell(pcc, month)}
-                    onMouseDown={() => mouseDown(pcc, month)}
-                    onMouseEnter={() => mouseOver(pcc, month)}
-                    //onMouseOut={() => mouseOut(pcc, month)}
-                    //onMouseUp={() => mouseUp(pcc, month)}
+                    //onClick={() => handleCurrentCell(row, col)}
+                    onMouseDown={() => mouseDown(row, col)}
+                    onMouseEnter={() => mouseOver(row, col)}
+                    //onMouseOut={() => mouseOut(row, col)}
+                    //onMouseUp={() => mouseUp(row, col)}
                 >
-                    {data[pcc][month]}
-                    {selectCell[pcc + month] && (
-                        <div className='td_corner'></div>
-                    )}
+                    {data[row][col]}
+                    {selectCell[row + col] && <div className='td_corner'></div>}
                 </td>
             </React.Fragment>
         );
@@ -113,25 +111,25 @@ function App() {
             <table className='table'>
                 <thead>
                     <tr>
-                        <th> Year</th>
-                        <th>Jan</th>
-                        <th>Feb</th>
-                        <th>Mar</th>
-                        <th>Arp</th>
-                        <th>May</th>
-                        <th>Jun</th>
+                        <th> </th>
+                        <th>A</th>
+                        <th>B</th>
+                        <th>C</th>
+                        <th>D</th>
+                        <th>E</th>
+                        <th>F</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {Object.keys(data).map((pcc, index) => (
+                    {Object.keys(data).map((row, index) => (
                         <tr key={index}>
-                            <td>{pcc}</td>
-                            <CustomCell pcc={pcc} month='01' />
-                            <CustomCell pcc={pcc} month='02' />
-                            <CustomCell pcc={pcc} month='03' />
-                            <CustomCell pcc={pcc} month='04' />
-                            <CustomCell pcc={pcc} month='05' />
-                            <CustomCell pcc={pcc} month='06' />
+                            <td>{row}</td>
+                            <CustomCell row={row} col='A' />
+                            <CustomCell row={row} col='B' />
+                            <CustomCell row={row} col='C' />
+                            <CustomCell row={row} col='D' />
+                            <CustomCell row={row} col='E' />
+                            <CustomCell row={row} col='F' />
                         </tr>
                     ))}
                 </tbody>
